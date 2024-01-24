f727f5161ebfd70502258cf24ad7e5843eb9b315
Shonster88 <bluvulture@gmail.com>
2024-01-24 09:39:23 +0100

    Initial commit

---
+++ src/Installer.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
+ */
+
+namespace Pimcore\Bundle\xTemplateBundlex;
+
+use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
+
+/**
+ * @internal
+ */
+final class Installer extends SettingsStoreAwareInstaller
+{
+}
---
+++ src/PimcorexTemplateBundlex.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
+ */
+
+namespace Pimcore\Bundle\xTemplateBundlex;
+
+use Pimcore\Bundle\EnterpriseSubscriptionToolsBundle\Bundle\EnterpriseBundleInterface;
+use Pimcore\Bundle\EnterpriseSubscriptionToolsBundle\PimcoreEnterpriseSubscriptionToolsBundle;
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\HttpKernel\BundleCollection\BundleCollection;
+
+class PimcorexTemplateBundlex extends AbstractPimcoreBundle implements
+    EnterpriseBundleInterface
+{
+    private const ENTERPRISE_BUNDLE_LICENSE_ID = 'xLicensex';
+
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    public function getJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getCssPaths(): array
+    {
+        return [];
+    }
+
+    public function getInstaller(): ?InstallerInterface
+    {
+        parent::getInstaller();
+
+        /** @var InstallerInterface|null */
+        return $this->container->get(Installer::class);
+    }
+
+    public static function registerDependentBundles(BundleCollection $collection): void
+    {
+        $collection->addBundle(new PimcoreEnterpriseSubscriptionToolsBundle());
+    }
+
+    public function getBundleLicenseId(): string
+    {
+        return self::ENTERPRISE_BUNDLE_LICENSE_ID;
+    }
+}
