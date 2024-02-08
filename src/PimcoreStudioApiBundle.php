5d29b4091dad7869bfe18d7553fb994b782416f6
Christian Fasching <fashxp@users.noreply.github.com>
2024-02-08 10:43:03 +0100

    remove enterprise subscription tools (#3)
    * remove enterprise subscription tools
    
    * remove enterprise subscription tools
    
    * Apply php-cs-fixer changes

--- src/PimcoreStudioApiBundle.php
+++ src/PimcoreStudioApiBundle.php
@@ -13,17 +13,11 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StudioApiBundle;
 
-use Pimcore\Bundle\EnterpriseSubscriptionToolsBundle\Bundle\EnterpriseBundleInterface;
-use Pimcore\Bundle\EnterpriseSubscriptionToolsBundle\PimcoreEnterpriseSubscriptionToolsBundle;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\Installer\InstallerInterface;
-use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 
-class PimcoreStudioApiBundle extends AbstractPimcoreBundle implements
-    EnterpriseBundleInterface
+class PimcoreStudioApiBundle extends AbstractPimcoreBundle
 {
-    private const ENTERPRISE_BUNDLE_LICENSE_ID = 'xLicensex';
-
     public function getPath(): string
     {
         return dirname(__DIR__);
@@ -45,15 +39,5 @@ class PimcoreStudioApiBundle extends AbstractPimcoreBundle implements
 
         /** @var InstallerInterface|null */
         return $this->container->get(Installer::class);
-    }
-
-    public static function registerDependentBundles(BundleCollection $collection): void
-    {
-        $collection->addBundle(new PimcoreEnterpriseSubscriptionToolsBundle());
-    }
-
-    public function getBundleLicenseId(): string
-    {
-        return self::ENTERPRISE_BUNDLE_LICENSE_ID;
     }
 }
