88590df43b5a00f9bd3e2c30e7e1c8fc0292d455
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2024-05-14 13:55:31 +0200

    Add text controller and encoder (#61)
    * Add Text preview controller and encoder
    
    * Apply php-cs-fixer changes
    
    * Add declare strict
    
    * Remove preview
    
    * Remove unused use statements
    
    * Add max file size exception
    
    * Add Encoder Tests
    
    ---------
    
    Co-authored-by: mattamon <mattamon@users.noreply.github.com>

---
+++ src/Asset/Encoder/TextEncoderInterface.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Asset\Encoder;
+
+use Pimcore\Bundle\StudioBackendBundle\Exception\InvalidElementTypeException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\MaxFileSizeExceededException;
+use Pimcore\Model\Element\ElementInterface;
+
+interface TextEncoderInterface
+{
+    /**
+     * @throws InvalidElementTypeException|MaxFileSizeExceededException
+     */
+    public function encodeUTF8(ElementInterface $element): string;
+}
