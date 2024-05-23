d10bfa3eeabeafa729ea51afffa1e59cdb8d77aa
alexz707 <alexz707@users.noreply.github.com>
2024-05-23 15:32:24 +0000

    Apply php-cs-fixer changes

--- src/Tag/Repository/TagRepositoryInterface.php
+++ src/Tag/Repository/TagRepositoryInterface.php
@@ -1,6 +1,19 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\StudioBackendBundle\Tag\Repository;
 
 use Pimcore\Bundle\StudioBackendBundle\Tag\Request\CreateTagParameters;
@@ -15,7 +28,10 @@ use Pimcore\Model\Element\Tag\Listing as TagListing;
 interface TagRepositoryInterface
 {
     public function getTagById(int $id): Tag;
+
     public function listTags(TagsParameters $parameters): TagListing;
+
     public function addTag(CreateTagParameters $params): Tag;
+
     public function updateTag(int $id, UpdateTagParameters $params): Tag;
 }
