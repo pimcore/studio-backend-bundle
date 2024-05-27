8c63e09cb8bfa3eab9cecb4614727cda4d1e60a5
mattamon <mattamon@users.noreply.github.com>
2024-05-27 06:59:30 +0000

    Apply php-cs-fixer changes

--- src/Asset/UpdateAdapter/PropertiesUpdateAdapter.php
+++ src/Asset/UpdateAdapter/PropertiesUpdateAdapter.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\StudioBackendBundle\Asset\UpdateAdapter;
 
 use Pimcore\Model\Element\ElementInterface;
@@ -8,10 +21,11 @@ use Pimcore\Model\Property;
 final class PropertiesUpdateAdapter implements UpdateAdapterInterface
 {
     private const DATA_INDEX = 'properties';
+
     public function update(ElementInterface $element, array $data): void
     {
         $properties = [];
-        foreach($data as $propertyData) {
+        foreach ($data as $propertyData) {
             $property = new Property();
             $property->setType($propertyData['type']);
             $property->setName($propertyData['key']);
