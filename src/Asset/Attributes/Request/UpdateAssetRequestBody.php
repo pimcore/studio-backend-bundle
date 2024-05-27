4d20a61ad0f78ecab38a6cbfdb4daf5d3a2a2da9
mattamon <mattamon@users.noreply.github.com>
2024-05-27 08:34:40 +0000

    Apply php-cs-fixer changes

--- src/Asset/Attributes/Request/UpdateAssetRequestBody.php
+++ src/Asset/Attributes/Request/UpdateAssetRequestBody.php
@@ -39,7 +39,7 @@ final class UpdateAssetRequestBody extends RequestBody
                             new UpdateElementProperties(),
                         ],
                         type: 'object',
-                    )
+                    ),
                 ],
                 type: 'object',
             ),
