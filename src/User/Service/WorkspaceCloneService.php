69589c0ad5d6358d1c96110b71dac50413b4461f
Martin Eiber <martin.eiber@pimcore.com>
2024-06-11 15:20:57 +0200

    Fix Format.

--- src/User/Service/WorkspaceCloneService.php
+++ src/User/Service/WorkspaceCloneService.php
@@ -60,7 +60,8 @@ final class WorkspaceCloneService implements WorkspaceCloneServiceInterface
      * @throws Exception
      */
     private function applyObjectVars(
-        AssetWorkspace|DocumentWorkspace|DataObjectWorkspace $workspace, array $objectVars
+        AssetWorkspace|DocumentWorkspace|DataObjectWorkspace $workspace,
+        array $objectVars
     ): AssetWorkspace|DocumentWorkspace|DataObjectWorkspace {
         foreach ($objectVars as $key => $value) {
             $workspace->setObjectVar($key, $value);
