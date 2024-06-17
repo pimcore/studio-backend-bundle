7d13915e37664c252373e18c62c4c2638372a138
Martin Eiber <martin.eiber@pimcore.com>
2024-06-17 16:22:33 +0200

    Initial get User Role.

---
+++ src/User/Schema/UserRole.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\User\Schema;
+
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
+use Pimcore\Bundle\StudioBackendBundle\Util\Schema\AdditionalAttributesInterface;
+use Pimcore\Bundle\StudioBackendBundle\Util\Traits\AdditionalAttributesTrait;
+
+/**
+ * @internal
+ */
+#[Schema(
+    title: 'User Role',
+    description: 'A user role which is a combination of permissions and settings.',
+    type: 'object'
+)]
+final class UserRole implements AdditionalAttributesInterface
+{
+    use AdditionalAttributesTrait;
+
+    public function __construct(
+        #[Property(description: 'ID of the Role', type: 'integer', example: '1')]
+        private readonly int $id,
+        #[Property(description: 'Name of the Role', type: 'string', example: 'role')]
+        private readonly string $category,
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+}
