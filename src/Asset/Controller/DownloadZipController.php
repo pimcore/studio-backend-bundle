08f9eaebec1375e2a98f998e6e9c5174bae1fa76
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2024-06-19 16:18:56 +0200

    Asset download with GEE (#153)
    * Setup GEE and assets download
    
    * Apply php-cs-fixer changes
    
    * Add GEE to bundles
    
    * 11.x not 1.x
    
    * Consistency check
    
    * Prototype susbcriber
    
    * Apply php-cs-fixer changes
    
    * Add download controller
    
    * Apply php-cs-fixer changes
    
    * Adding TODOs
    
    * Apply php-cs-fixer changes
    
    * Update interface
    
    * Remove unused repo
    
    * Apply php-cs-fixer changes
    
    * Add space
    
    * Use stop on first error since we do not want incomplete zips
    
    ---------
    
    Co-authored-by: mattamon <mattamon@users.noreply.github.com>

---
+++ src/Asset/Controller/DownloadZipController.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Asset\Controller;
+
+use OpenApi\Attributes\Get;
+use Pimcore\Bundle\StudioBackendBundle\Asset\Attributes\Response\Content\AssetMediaType;
+use Pimcore\Bundle\StudioBackendBundle\Asset\Attributes\Response\Header\ContentDisposition;
+use Pimcore\Bundle\StudioBackendBundle\Asset\Service\DownloadServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
+use Pimcore\Bundle\StudioBackendBundle\ExecutionEngine\MappedParameter\ZipPathParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Parameters\Query\PathParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\DefaultResponses;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\SuccessResponse;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\UserPermissions;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @internal
+ */
+final class DownloadZipController extends AbstractApiController
+{
+    public function __construct(
+        SerializerInterface $serializer,
+        private readonly DownloadServiceInterface $downloadService
+    ) {
+        parent::__construct($serializer);
+    }
+
+    #[Route('/assets/download/zip', name: 'pimcore_studio_api_zip_download_asset', methods: ['GET'])]
+    #[IsGranted(UserPermissions::ASSETS->value)]
+    #[Get(
+        path: self::API_PATH . '/assets/download/zip',
+        operationId: 'downloadZippedAssets',
+        description: 'Downloading zipped assets',
+        summary: 'Downloading the zip file with assets',
+        tags: [Tags::Assets->name]
+    )]
+    #[PathParameter]
+    #[SuccessResponse(
+        description: 'Zip archive',
+        content: [new AssetMediaType('application/zip')],
+        headers: [new ContentDisposition()]
+    )]
+    #[DefaultResponses([
+        HttpResponseCodes::UNAUTHORIZED,
+        HttpResponseCodes::NOT_FOUND,
+    ])]
+    public function downloadZippedAssets(#[MapQueryString] ZipPathParameter $path): StreamedResponse
+    {
+        return $this->downloadService->downloadZipArchiveByPath($path);
+    }
+}
---
+++ src/ExecutionEngine/AutomationAction/Messenger/Handler/ZipCreationHandler.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\ExecutionEngine\AutomationAction\Messenger\Handler;
+
+use Exception;
+use Pimcore\Bundle\GenericExecutionEngineBundle\Messenger\Handler\AbstractAutomationActionHandler;
+use Pimcore\Bundle\StaticResolverBundle\Models\Element\ServiceResolverInterface;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\ExecutionEngine\AutomationAction\Messenger\Messages\ZipCreationMessage;
+use Pimcore\Bundle\StudioBackendBundle\ExecutionEngine\Service\ZipServiceInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+/**
+ * @internal
+ */
+#[AsMessageHandler]
+final class ZipCreationHandler extends AbstractAutomationActionHandler
+{
+    public function __construct(
+        private readonly ServiceResolverInterface $serviceResolver,
+        private readonly ZipServiceInterface $zipService
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function __invoke(ZipCreationMessage $message): void
+    {
+        $jobRun = $this->getJobRun($message);
+
+        $context = $jobRun->getContext();
+
+        if (!array_key_exists(ZipServiceInterface::ASSETS_INDEX, $context)) {
+            $this->abortAction(
+                'no_assets_found',
+                [],
+                'studio_backend',
+                NotFoundException::class
+            );
+        }
+
+        $archive = $this->zipService->getZipArchive($jobRun->getId());
+
+        if (!$archive) {
+            $this->abortAction(
+                'zip_archive_not_found',
+                [],
+                'studio_backend',
+                NotFoundException::class
+            );
+        }
+
+        foreach ($context[ZipServiceInterface::ASSETS_INDEX] as $id) {
+
+            $asset = $this->serviceResolver->getElementById('asset', $id);
+
+            if (!$asset) {
+                $this->abortAction(
+                    'asset_not_found',
+                    [],
+                    'studio_backend',
+                    NotFoundException::class
+                );
+            }
+
+            $this->zipService->addFile($archive, $asset);
+            // TODO Send SSE for percentage update
+        }
+
+        $archive->close();
+    }
+}
