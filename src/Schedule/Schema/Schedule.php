7f1542438143778ef6a1361df9badb0a63a0191a
mattamon <matthias.schuhmayer@pimcore.com>
2024-06-19 15:59:34 +0200

    Only use publish instead of publish-version to make it consistent with delete action

--- src/Schedule/Schema/Schedule.php
+++ src/Schedule/Schema/Schedule.php
@@ -40,7 +40,7 @@ final class Schedule implements AdditionalAttributesInterface
         private readonly string $ctype,
         #[Property(description: 'Date of schedule', type: 'integer', example: 1634025600)]
         private readonly int $date,
-        #[Property(description: 'Action', type: 'string', enum: ['publish-version', 'delete'])]
+        #[Property(description: 'Action', type: 'string', enum: ['publish', 'delete'])]
         private readonly ?string $action,
         #[Property(description: 'Version ID', type: 'integer', example: 987)]
         private readonly ?int $version,
