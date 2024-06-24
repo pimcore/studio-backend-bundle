9ec69b5082faa42ca42497642e2ea5f4f8ec0551
Martin Eiber <martin.eiber@gmail.com>
2024-06-24 15:27:37 +0200

    #139 [User Management] [Endpoint] Update a specific User (#171)
    * Initial Update User
    
    * Apply php-cs-fixer changes
    
    * Remove unused getter.
    
    * Make array_map static.
    
    ---------
    
    Co-authored-by: martineiber <martineiber@users.noreply.github.com>

---
+++ src/User/Controller/UpdateUserController.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\User\Controller;
+
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Put;
+use OpenApi\Attributes\RequestBody;
+use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\DatabaseException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ForbiddenException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ParseException;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Parameters\Path\IdParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\DefaultResponses;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\SuccessResponse;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\User\MappedParameter\UpdateUserParameter;
+use Pimcore\Bundle\StudioBackendBundle\User\Schema\UpdateUser;
+use Pimcore\Bundle\StudioBackendBundle\User\Schema\User as UserSchema;
+use Pimcore\Bundle\StudioBackendBundle\User\Service\UserUpdateServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\UserPermissions;
+use Pimcore\Bundle\StudioBackendBundle\Util\Traits\PaginatedResponseTrait;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @internal
+ */
+final class UpdateUserController extends AbstractApiController
+{
+    use PaginatedResponseTrait;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        private readonly UserUpdateServiceInterface $userUpdateService
+    ) {
+        parent::__construct($serializer);
+    }
+
+    /**
+     * @throws NotFoundException|DatabaseException|ForbiddenException|ParseException
+     */
+    #[Route('/user/{id}', name: 'pimcore_studio_api_user_update', methods: ['PUT'])]
+    #[IsGranted(UserPermissions::USER_MANAGEMENT->value)]
+    #[Put(
+        path: self::API_PATH . '/user/{id}',
+        operationId: 'updateUserById',
+        summary: 'Update user by id.',
+        tags: [Tags::User->value]
+    )]
+    #[IdParameter]
+    #[SuccessResponse(
+        description: 'Updated data.',
+        content: new JsonContent(ref: UserSchema::class)
+    )]
+    #[RequestBody(
+        content: new JsonContent(ref: UpdateUser::class)
+    )]
+    #[DefaultResponses([
+        HttpResponseCodes::NOT_FOUND,
+    ])]
+    public function updateUsers(int $id, #[MapRequestPayload] UpdateUserParameter $userUpdate): JsonResponse
+    {
+        return $this->jsonResponse(
+            $this->userUpdateService->updateUserById($userUpdate, $id)
+        );
+    }
+}
---
+++ src/User/Schema/UpdateUser.php
@@ -0,0 +1,212 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\User\Schema;
+
+use OpenApi\Attributes\Items;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
+
+/**
+ * @internal
+ */
+#[Schema(
+    title: 'User',
+    description: 'User Schema to update a User.',
+    required: [
+        'active', 'classes', 'closeWarning', 'allowDirtyClose', 'contentLanguages', 'keyBindings',
+        'language', 'memorizeTabs', 'parentId', 'permissions', 'roles', 'twoFactorAuthenticationEnabled',
+        'websiteTranslationLanguagesEdit', 'websiteTranslationLanguagesView', 'welcomeScreen',
+        'assetWorkspaces', 'dataObjectWorkspaces', 'documentWorkspaces',
+    ],
+    type: 'object'
+)]
+final readonly class UpdateUser
+{
+    public function __construct(
+        #[Property(description: 'Email of the User', type: 'string', example: '')]
+        private ?string $email,
+        #[Property(description: 'Firstname of the User', type: 'string', example: '')]
+        private ?string $firstname,
+        #[Property(description: 'Lastname of the User', type: 'string', example: '')]
+        private ?string $lastname,
+        #[Property(description: 'If User is admin', type: 'boolean', example: false)]
+        private bool $admin,
+        #[Property(description: 'If User is active', type: 'boolean', example: true)]
+        private bool $active,
+        #[Property(description: 'Classes the user is allows to see', type: 'object', example: ['CAR'])]
+        private array $classes,
+        #[Property(type: 'boolean', example: true)]
+        private bool $closeWarning,
+        #[Property(type: 'boolean', example: true)]
+        private bool $allowDirtyClose,
+        #[Property(
+            description: 'List of available content Language already sorted.',
+            type: 'object',
+            example: ['de', 'en']
+        )]
+        private array $contentLanguages,
+        #[Property(description: 'Key Bindings', type: 'array', items: new Items(ref: KeyBinding::class))]
+        private array $keyBindings,
+        #[Property(description: 'Language of the User', type: 'string', example: 'de')]
+        private string $language,
+        #[Property(type: 'boolean', example: true)]
+        private bool $memorizeTabs,
+        #[Property(type: 'int', example: '2')]
+        private int $parentId,
+        #[Property(description: 'List of permissions for the user', type: 'object', example: ['objects', 'documents'])]
+        private array $permissions,
+        #[Property(description: 'ID List of roles the user is assigned', type: 'object', example: [12, 14])]
+        private array $roles,
+        #[Property(type: 'boolean', example: false)]
+        private bool $twoFactorAuthenticationEnabled,
+        #[Property(type: 'object', example: ['de', 'en'])]
+        private array $websiteTranslationLanguagesEdit,
+        #[Property(type: 'object', example: ['de'])]
+        private array $websiteTranslationLanguagesView,
+        #[Property(type: 'boolean', example: true)]
+        private bool $welcomeScreen,
+        #[Property(description: 'Asset Workspace', type: 'array', items: new Items(ref: UserWorkspace::class))]
+        private array $assetWorkspaces,
+        #[Property(description: 'Data Object Workspace', type: 'array', items: new Items(ref: UserWorkspace::class))]
+        private array $dataObjectWorkspaces,
+        #[Property(description: 'Document Workspace', type: 'array', items: new Items(ref: UserWorkspace::class))]
+        private array $documentWorkspaces,
+    ) {
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->admin;
+    }
+
+    public function getClasses(): array
+    {
+        return $this->classes;
+    }
+
+    public function isCloseWarning(): bool
+    {
+        return $this->closeWarning;
+    }
+
+    public function getContentLanguages(): array
+    {
+        return $this->contentLanguages;
+    }
+
+    /**
+     * @return KeyBinding[]
+     */
+    public function getKeyBindings(): array
+    {
+        return $this->keyBindings;
+    }
+
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    public function isMemorizeTabs(): bool
+    {
+        return $this->memorizeTabs;
+    }
+
+    public function getParentId(): int
+    {
+        return $this->parentId;
+    }
+
+    public function getPermissions(): array
+    {
+        return $this->permissions;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function isAllowDirtyClose(): bool
+    {
+        return $this->allowDirtyClose;
+    }
+
+    public function isTwoFactorAuthenticationEnabled(): bool
+    {
+        return $this->twoFactorAuthenticationEnabled;
+    }
+
+    public function getWebsiteTranslationLanguagesEdit(): array
+    {
+        return $this->websiteTranslationLanguagesEdit;
+    }
+
+    public function getWebsiteTranslationLanguagesView(): array
+    {
+        return $this->websiteTranslationLanguagesView;
+    }
+
+    public function getWelcomeScreen(): bool
+    {
+        return $this->welcomeScreen;
+    }
+
+    /**
+     * @return UserWorkspace[]
+     */
+    public function getAssetWorkspaces(): array
+    {
+        return $this->assetWorkspaces;
+    }
+
+    /**
+     * @return UserWorkspace[]
+     */
+    public function getDataObjectWorkspaces(): array
+    {
+        return $this->dataObjectWorkspaces;
+    }
+
+    /**
+     * @return UserWorkspace[]
+     */
+    public function getDocumentWorkspaces(): array
+    {
+        return $this->documentWorkspaces;
+    }
+}
