7cd2b33322cbb9b0d1e868e53f2d5d5095845183
lukmzig <30526586+lukmzig@users.noreply.github.com>
2024-07-02 13:48:11 +0200

    [Asset versions] Check if asset version download endpoint should deliver thumbnail or original asset (#209)
    * add original version stream preview for PDF
    
    * fix: remove unnecessary check
    
    * fix: use imports

---
+++ src/Version/Controller/Asset/PdfStreamController.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Version\Controller\Asset;
+
+use OpenApi\Attributes\Get;
+use Pimcore\Bundle\StudioBackendBundle\Asset\Attributes\Response\Content\AssetMediaType;
+use Pimcore\Bundle\StudioBackendBundle\Asset\Attributes\Response\Header\ContentDisposition;
+use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\AccessDeniedException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ElementProcessingNotCompletedException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ElementStreamResourceNotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\EnvironmentException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidElementTypeException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UnprocessableContentException;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Parameters\Path\IdParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\DefaultResponses;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\SuccessResponse;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\Security\Service\SecurityServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\HttpResponseHeaders;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\UserPermissions;
+use Pimcore\Bundle\StudioBackendBundle\Version\Service\VersionBinaryServiceInterface;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @internal
+ */
+final class PdfStreamController extends AbstractApiController
+{
+    public function __construct(
+        SerializerInterface $serializer,
+        private readonly SecurityServiceInterface $securityService,
+        private readonly VersionBinaryServiceInterface $versionBinaryService,
+    ) {
+        parent::__construct($serializer);
+    }
+
+    /**
+     * @throws AccessDeniedException
+     * @throws ElementProcessingNotCompletedException
+     * @throws ElementStreamResourceNotFoundException
+     * @throws EnvironmentException
+     * @throws InvalidElementTypeException
+     * @throws NotFoundException
+     * @throws UnprocessableContentException
+     */
+    #[Route('/versions/{id}/pdf/stream', name: 'pimcore_studio_api_stream_pdf_version', methods: ['GET'])]
+    #[IsGranted(UserPermissions::ASSETS->value)]
+    #[Get(
+        path: self::API_PATH . '/versions/{id}/pdf/stream',
+        operationId: 'streamPdfVersionById',
+        description: 'Get PDF version stream based on the version ID',
+        summary: 'Get PDF version stream by ID',
+        tags: [Tags::Versions->name]
+    )]
+    #[IdParameter(type: 'version')]
+    #[SuccessResponse(
+        description: 'PDF version stream',
+        content: new AssetMediaType('application/pdf'),
+        headers: [new ContentDisposition(HttpResponseHeaders::INLINE_TYPE->value)]
+    )]
+    #[DefaultResponses([
+        HttpResponseCodes::UNAUTHORIZED,
+        HttpResponseCodes::NOT_FOUND,
+    ])]
+    public function streamPdfVersionById(int $id): StreamedResponse
+    {
+        return $this->versionBinaryService->streamPdfPreview($id, $this->securityService->getCurrentUser());
+    }
+}
