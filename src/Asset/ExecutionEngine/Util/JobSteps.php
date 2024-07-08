4a463cae2e5b87168e951a7f0f92ee1189c80dcf
lukmzig <lukas.mzigot@pimcore.com>
2024-07-08 09:55:37 +0200

    remove unused const

--- src/Asset/ExecutionEngine/Util/JobSteps.php
+++ src/Asset/ExecutionEngine/Util/JobSteps.php
@@ -20,7 +20,6 @@ enum JobSteps: string
     case ZIP_COLLECTION = 'studio_ee_job_step_zip_collection';
     case ZIP_CREATION = 'studio_ee_job_step_zip_creation';
     case ZIP_UPLOADING = 'studio_ee_jop_step_zip_uploading';
-    case ZIP_CLEANUP = 'studio_ee_job_step_zip_cleanup';
     case ASSET_DELETION = 'studio_ee_job_step_asset_deletion';
     case ASSET_CLONING = 'studio_ee_job_step_asset_cloning';
     case ASSET_UPLOADING = 'studio_ee_job_step_asset_uploading';
