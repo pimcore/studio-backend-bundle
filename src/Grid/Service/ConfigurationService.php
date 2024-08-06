2009833918598c1db87f1aa7b32e6e0e7203e2a3
Martin Eiber <martin.eiber@pimcore.com>
2024-08-06 16:17:15 +0200

    Style.

--- src/Grid/Service/ConfigurationService.php
+++ src/Grid/Service/ConfigurationService.php
@@ -48,7 +48,10 @@ final readonly class ConfigurationService implements ConfigurationServiceInterfa
             }
 
             // rather use the spread operator instead of array_merge in a loop
-            $columns = [...$columns, ...$collector->getColumnConfigurations($this->gridService->getColumnDefinitions())];
+            $columns = [
+                ...$columns,
+                ...$collector->getColumnConfigurations($this->gridService->getColumnDefinitions())
+            ];
         }
 
         foreach ($columns as $column) {
