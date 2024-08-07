546e5e6f01d6c14c788e4e6e9722756330a93e64
lukmzig <lukas.mzigot@pimcore.com>
2024-08-07 14:19:57 +0200

    add replace content functionality

---
+++ src/DataObject/Controller/ReplaceController.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\DataObject\Controller;
+
+use OpenApi\Attributes\Post;
+use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
+use Pimcore\Bundle\StudioBackendBundle\DataObject\Service\ReplaceServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\AccessDeniedException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ElementSavingFailedException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidElementTypeException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UserNotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Parameters\Path\IdParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\DefaultResponses;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attributes\Response\SuccessResponse;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\ElementTypes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constants\UserPermissions;
+use Pimcore\Bundle\StudioBackendBundle\Util\Traits\PaginatedResponseTrait;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @internal
+ */
+final class ReplaceController extends AbstractApiController
+{
+    use PaginatedResponseTrait;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        private readonly ReplaceServiceInterface $replaceService,
+    ) {
+        parent::__construct($serializer);
+    }
+
+    /**
+     * @throws AccessDeniedException
+     * @throws ElementSavingFailedException
+     * @throws InvalidElementTypeException
+     * @throws UserNotFoundException
+     * @throws NotFoundException
+     */
+    #[Route(
+        '/data-objects/{sourceId}/replace/{targetId}',
+        name: 'pimcore_studio_api_data_objects_replace',
+        methods: ['POST']
+    )]
+    #[IsGranted(UserPermissions::DATA_OBJECTS->value)]
+    #[Post(
+        path: self::API_PATH . '/data-objects/{sourceId}/replace/{targetId}',
+        operationId: 'replaceContent',
+        summary: 'Replace content of a specific data object.',
+        tags: [Tags::DataObjects->value]
+    )]
+    #[SuccessResponse(
+        description: 'Successfully replaced data object content',
+    )]
+    #[IdParameter(type: ElementTypes::TYPE_DATA_OBJECT, name: 'sourceId')]
+    #[IdParameter(type: ElementTypes::TYPE_DATA_OBJECT, name: 'targetId')]
+    #[DefaultResponses([
+        HttpResponseCodes::UNAUTHORIZED,
+        HttpResponseCodes::NOT_FOUND,
+    ])]
+    public function replaceContent(
+        int $sourceId,
+        int $targetId,
+    ): Response {
+        $this->replaceService->replaceContents($sourceId, $targetId);
+
+        return new Response();
+    }
+}
