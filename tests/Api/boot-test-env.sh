f4de8791f634c703c8d996c7e37109d057b47384
Martin Eiber <martin.eiber@pimcore.com>
2024-09-02 07:51:50 +0200

    #400 Change naming.

--- tests/Api/boot-test-env.sh
+++ tests/Api/boot-test-env.sh
@@ -95,7 +95,7 @@ printf "%s${NC}\n"
 
 
 ###############################################################################
-# Change security.yaml ? or Just copy a final.yaml file?
+# Change security.yaml ? or Just copy a predefined .yaml file?
 ###############################################################################
 printf "    ${Yellow}Change${NC} config settings in ${Cyan}${TARGET_DIR}/config/packages/security.yaml${NC}"
     sed -i '/firewalls:/a\        pimcore_studio: '\''%pimcore_studio_backend.firewall_settings%'\''' ./config/packages/security.yaml
