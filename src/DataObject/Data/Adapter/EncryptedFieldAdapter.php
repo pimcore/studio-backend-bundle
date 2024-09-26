fd9eb55780fbf69a438b40b2e8fb16b695057cde
Marco Perberschlager <marco.perberschlager@pimcore.com>
2024-09-26 15:23:16 +0200

    Added EncryptedField

---
+++ src/DataObject/Data/Adapter/EncryptedFieldAdapter.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\DataObject\Data\Adapter;
+
+use Pimcore\Bundle\StudioBackendBundle\DataObject\Data\SetterDataInterface;
+use Pimcore\Bundle\StudioBackendBundle\DataObject\Service\DataAdapterLoaderInterface;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
+use Pimcore\Model\DataObject\Concrete;
+use Pimcore\Model\DataObject\Data\EncryptedField;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+/**
+ * @internal
+ */
+#[AutoconfigureTag(DataAdapterLoaderInterface::ADAPTER_TAG)]
+final readonly class EncryptedFieldAdapter implements SetterDataInterface
+{
+    public function getDataForSetter(
+        Concrete $element,
+        Data $fieldDefinition,
+        string $key,
+        array $data
+    ): ?EncryptedField
+    {
+        $value = $data[$key];
+
+        if(!($fieldDefinition instanceof Data\EncryptedField)) {
+            return null;
+        }
+
+        $fd = $fieldDefinition->getDelegateDatatypeDefinition();
+        if(!$fd) {
+            return null;
+        }
+        $result = $fd->getDataFromEditmode($value, $element);
+
+        return new EncryptedField($fieldDefinition->getDelegate(), $result);
+    }
+}
