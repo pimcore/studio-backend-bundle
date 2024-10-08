ab263cc3af7aef7184d859a3cfebf195e4d4bc58
mattamon <matthias.schuhmayer@pimcore.com>
2024-10-08 12:45:07 +0200

    Update interface

--- src/DataIndex/Provider/AssetQueryProvider.php
+++ src/DataIndex/Provider/AssetQueryProvider.php
@@ -18,6 +18,7 @@ namespace Pimcore\Bundle\StudioBackendBundle\DataIndex\Provider;
 
 use Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService\SearchProviderInterface;
 use Pimcore\Bundle\StudioBackendBundle\DataIndex\Query\AssetQuery;
+use Pimcore\Bundle\StudioBackendBundle\DataIndex\Query\AssetQueryInterface;
 use Pimcore\Bundle\StudioBackendBundle\DataIndex\Query\QueryInterface;
 
 final readonly class AssetQueryProvider implements AssetQueryProviderInterface
@@ -26,7 +27,7 @@ final readonly class AssetQueryProvider implements AssetQueryProviderInterface
     {
     }
 
-    public function createAssetQuery(): QueryInterface
+    public function createAssetQuery(): AssetQueryInterface
     {
         return new AssetQuery($this->searchProvider->createAssetSearch());
     }
