af39af8a8e92e91f6d5566760385a646131ba8cc
martineiber <martineiber@users.noreply.github.com>
2024-11-05 14:47:52 +0000

    Apply php-cs-fixer changes

--- src/DataIndex/Filter/IdFilter.php
+++ src/DataIndex/Filter/IdFilter.php
@@ -20,7 +20,7 @@ use Pimcore\Bundle\StudioBackendBundle\DataIndex\Query\QueryInterface;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidArgumentException;
 use Pimcore\Bundle\StudioBackendBundle\Grid\Column\ColumnType;
 use Pimcore\Bundle\StudioBackendBundle\MappedParameter\Filter\SimpleColumnFiltersParameterInterface;
-use function is_integer;
+use function is_int;
 
 /**
  * @internal
