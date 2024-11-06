a0c736abfe5603379c811501d0a9b5abbf453cc6
Martin Eiber <martin.eiber@pimcore.com>
2024-11-06 10:22:26 +0100

    Add ID Resolver

---
+++ src/Grid/Column/Resolver/System/IdResolver.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Grid\Column\Resolver\System;
+
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidArgumentException;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Column\ColumnResolverInterface;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Column\ColumnType;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Schema\Column;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Schema\ColumnData;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Util\Trait\ColumnDataTrait;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Util\Trait\SimpleGetterTrait;
+use Pimcore\Bundle\StudioBackendBundle\Response\ElementInterface;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\ElementTypes;
+
+/**
+ * @internal
+ */
+final class IdResolver implements ColumnResolverInterface
+{
+    use SimpleGetterTrait;
+    use ColumnDataTrait;
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function resolve(Column $column, ElementInterface $element): ColumnData
+    {
+        return $this->getColumnData(
+            $column,
+            $this->getValue($column, $element)
+        );
+    }
+
+    public function getType(): string
+    {
+        return ColumnType::SYSTEM_ID->value;
+    }
+
+    public function supportedElementTypes(): array
+    {
+        return [
+            ElementTypes::TYPE_ASSET,
+            ElementTypes::TYPE_DOCUMENT,
+            ElementTypes::TYPE_OBJECT,
+        ];
+    }
+}
