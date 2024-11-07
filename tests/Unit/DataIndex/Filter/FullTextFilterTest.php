d8aaf627799201d9736d8bf32cb6791348fb268d
martineiber <martineiber@users.noreply.github.com>
2024-11-07 10:46:15 +0000

    Apply php-cs-fixer changes

--- tests/Unit/DataIndex/Filter/FullTextFilterTest.php
+++ tests/Unit/DataIndex/Filter/FullTextFilterTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 /**
  * Pimcore
  *
- * This source file is available under following license:
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
  * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
 namespace Pimcore\Bundle\StudioBackendBundle\Tests\Unit\DataIndex\Filter;
@@ -16,7 +19,6 @@ namespace Pimcore\Bundle\StudioBackendBundle\Tests\Unit\DataIndex\Filter;
 use Codeception\Stub\Expected;
 use Codeception\Test\Unit;
 use Pimcore\Bundle\StudioBackendBundle\DataIndex\Filter\FullTextFilter;
-use Pimcore\Bundle\StudioBackendBundle\DataIndex\Query\AssetQueryInterface;
 use Pimcore\Bundle\StudioBackendBundle\DataIndex\Query\QueryInterface;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidArgumentException;
 use Pimcore\Bundle\StudioBackendBundle\MappedParameter\Filter\SimpleColumnFilter;
@@ -34,7 +36,6 @@ final class FullTextFilterTest extends Unit
             'getSimpleColumnFilterByType' => $columnFilter,
         ]);
 
-
         $query = $this->makeEmpty(QueryInterface::class);
 
         $this->expectException(InvalidArgumentException::class);
@@ -46,15 +47,14 @@ final class FullTextFilterTest extends Unit
 
     public function testIfFilterFullTextIsCalled(): void
     {
-        $columnFilter = new SimpleColumnFilter('system.fulltext', "term");
+        $columnFilter = new SimpleColumnFilter('system.fulltext', 'term');
         $parameter = $this->makeEmpty(SimpleColumnFiltersParameterInterface::class, [
             'getSimpleColumnFilterByType' => $columnFilter,
         ]);
 
-
         $query = $this->makeEmpty(QueryInterface::class, [
             'filterFullText' => Expected::once(function ($term) {
-                $this->assertSame("term", $term);
+                $this->assertSame('term', $term);
 
                 return $this->makeEmpty(QueryInterface::class);
             }),
