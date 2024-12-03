d63a80f80d90c5c3c32bd158646cad08931394f7
mattamon <matthias.schuhmayer@pimcore.com>
2024-12-03 15:29:36 +0100

    Add thumbnail clear

---
+++ src/Asset/Controller/Image/ImageClearController.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Thumbnail\Controller;
+
+use OpenApi\Attributes\Delete;
+use Pimcore\Bundle\StudioBackendBundle\Asset\Service\AssetServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UserNotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Parameter\Path\IdParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\DefaultResponses;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\SuccessResponse;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\Thumbnail\Service\ThumbnailServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\ElementTypes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\UserPermissions;
+use Pimcore\Bundle\StudioBackendBundle\Util\Trait\PaginatedResponseTrait;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @internal
+ */
+final class ImageClearController extends AbstractApiController
+{
+    use PaginatedResponseTrait;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        private readonly AssetServiceInterface $assetService
+    ) {
+        parent::__construct($serializer);
+    }
+
+    /**
+     * @throws UserNotFoundException
+     */
+    #[Route('/assets/{id}/image/thumbnail/clear', name: 'pimcore_studio_api_clear_image_thumbnail', methods: ['DELETE'])]
+    #[IsGranted(UserPermissions::ASSETS->value)]
+    #[IsGranted(UserPermissions::THUMBNAILS->value)]
+    #[Delete(
+        path: self::PREFIX . '/assets/{id}/image/thumbnail/clear',
+        operationId: 'asset_image_clear_thumbnail',
+        description: 'asset_image_clear_thumbnail_description',
+        summary: 'asset_image_clear_thumbnail_summary',
+        tags: [Tags::AssetThumbnails->value]
+    )]
+    #[IdParameter(type: ElementTypes::TYPE_ASSET)]
+    #[SuccessResponse]
+    #[DefaultResponses([
+        HttpResponseCodes::UNAUTHORIZED,
+        HttpResponseCodes::NOT_FOUND,
+        HttpResponseCodes::INTERNAL_SERVER_ERROR
+    ])]
+    public function clearImageThumbnails(int $id): Response
+    {
+        $this->assetService->clearThumbnails($id);
+
+        return new Response();
+    }
+}
