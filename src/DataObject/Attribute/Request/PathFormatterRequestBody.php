1297c832a0028329d7dfcf0d77490b2b62031d56
Martin Eiber <martin.eiber@pimcore.com>
2025-01-21 13:39:13 +0100

    Code Style

--- src/DataObject/Attribute/Request/PathFormatterRequestBody.php
+++ src/DataObject/Attribute/Request/PathFormatterRequestBody.php
@@ -36,7 +36,9 @@ final class PathFormatterRequestBody extends RequestBody
                     new Property(
                         property: 'targets',
                         type: 'object',
-                        example: '{"object_10":{"id":10,"type":"object","label":"/Product Data/Cars/jaguar/E-Type/coupé","path":"/Product Data/Cars/jaguar/E-Type/coupé","nicePathKey":"object_10"}}'
+                        example: '{"object_10":{"id":10,"type":"object","label":"/Product 
+                        Data/Cars/jaguar/E-Type/coupé","path":"/Product Data/Cars/jaguar/E-Type/coupé",
+                        "nicePathKey":"object_10"}}'
                     ),
                     new Property(
                         property: 'context',
