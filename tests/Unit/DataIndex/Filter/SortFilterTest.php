dd60b22a1a72e5ed84dbb3942520807acf09fdfe
Martin Eiber <martin.eiber@pimcore.com>
2025-02-07 07:21:07 +0100

    Move Test

--- tests/Unit/DataIndex/Filter/SortFilterTest.php
+++ tests/Unit/DataIndex/Filter/SortFilterTest.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-namespace Pimcore\Bundle\StudioBackendBundle\Tests\Unit\DataIndex\Filter\Asset;
+namespace Pimcore\Bundle\StudioBackendBundle\Tests\Unit\DataIndex\Filter;
 
 use Codeception\Stub\Expected;
 use Codeception\Test\Unit;
