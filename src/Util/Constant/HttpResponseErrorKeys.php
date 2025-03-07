e05f86e8884a4338968d33f7147b5a26ed026993
mattamon <matthias.schuhmayer@pimcore.com>
2025-03-07 08:10:30 +0100

    Adding specifc error key for not found exception

--- src/Util/Constant/HttpResponseErrorKeys.php
+++ src/Util/Constant/HttpResponseErrorKeys.php
@@ -24,6 +24,7 @@ enum HttpResponseErrorKeys: string
 
     case GENERIC_ERROR = 'error_something_generic_went_wrong';
     case ELEMENT_EXISTS = 'error_element_exists';
+    case ELEMENT_NOT_FOUND = 'error_element_not_found';
     case ELEMENT_VALIDATION_FAILED = 'error_element_validation_failed';
     case FOLDER_EXISTS = 'error_folder_exists';
     case CONFIG_NAME_INVALID = 'error_config_name_invalid';
