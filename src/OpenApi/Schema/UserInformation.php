c6f0235ef7b5c603b5ad12fa1df2171787e1941e
mcop1 <89011527+mcop1@users.noreply.github.com>
2025-03-18 15:27:12 +0100

    Added allowed classes and allowed doctypes (#913)
    * Added allowed classes and allowed doctypes
    
    * Renamed getter/property

--- src/OpenApi/Schema/UserInformation.php
+++ src/OpenApi/Schema/UserInformation.php
@@ -48,6 +48,18 @@ final class UserInformation implements AdditionalAttributesInterface
         private readonly array $permissions,
         #[Property(description: 'If user is an admin user', type: 'boolean', example: false)]
         private readonly bool $isAdmin,
+        #[Property(
+            description: 'Allowed classes to create',
+            type: 'array',
+            items: new Items(type: 'string')
+        )]
+        private readonly array $classes,
+        #[Property(
+            description: 'Allowed doc types to create',
+            type: 'array',
+            items: new Items(type: 'string')
+        )]
+        private readonly array $docTypes,
     ) {
     }
 
@@ -69,5 +81,15 @@ final class UserInformation implements AdditionalAttributesInterface
     public function getIsAdmin(): bool
     {
         return $this->isAdmin;
+    }
+
+    public function getClasses(): array
+    {
+        return $this->classes;
+    }
+
+    public function getDocTypes(): array
+    {
+        return $this->docTypes;
     }
 }
