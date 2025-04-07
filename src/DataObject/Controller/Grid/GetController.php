1703ede8ce0ebdddf3ee9f2b3e63bf4085b42dde
lukmzig <lukas.mzigot@pimcore.com>
2025-04-07 09:14:49 +0200

    fiX STAN

--- src/DataObject/Controller/Grid/GetController.php
+++ src/DataObject/Controller/Grid/GetController.php
@@ -24,7 +24,6 @@ use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
 use Pimcore\Bundle\StudioBackendBundle\Grid\Attribute\Property\GridCollection;
 use Pimcore\Bundle\StudioBackendBundle\Grid\Attribute\Request\GridRequestBody;
 use Pimcore\Bundle\StudioBackendBundle\Grid\MappedParameter\GridParameter;
-use Pimcore\Bundle\StudioBackendBundle\Grid\Service\ColumnConfigurationServiceInterface;
 use Pimcore\Bundle\StudioBackendBundle\Grid\Service\GridServiceInterface;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Parameter\Path\StringParameter;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\Content\CollectionJson;
@@ -46,8 +45,7 @@ final class GetController extends AbstractApiController
 {
     public function __construct(
         SerializerInterface $serializer,
-        private readonly GridServiceInterface $gridService,
-        private readonly ColumnConfigurationServiceInterface $columnConfigurationService
+        private readonly GridServiceInterface $gridService
     ) {
         parent::__construct($serializer);
     }
@@ -86,15 +84,6 @@ final class GetController extends AbstractApiController
     public function getDataObjectGrid(#[MapRequestPayload] GridParameter $gridParameter, string $classId): JsonResponse
     {
 
-        return $this->jsonResponse(
-            $this->gridService->getDataObjectGrid(
-                $gridParameter,
-                $this->columnConfigurationService->getAvailableDataObjectColumnConfiguration(
-                    $classId,
-                    $gridParameter->getFolderId()
-                ),
-                $classId
-            )
-        );
+        return $this->jsonResponse($this->gridService->getDataObjectGrid($gridParameter, $classId));
     }
 }
