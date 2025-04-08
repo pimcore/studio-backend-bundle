2eeaba0942aaaa91ca5ed6ce85d8a67e85ed8101
Lukas Mzigot <30526586+lukmzig@users.noreply.github.com>
2025-04-08 12:44:40 +0200

    Update src/Grid/Service/ClassDefinitionService.php
    Co-authored-by: Martin Eiber <martin.eiber@pimcore.com>

--- src/Grid/Service/ClassDefinitionService.php
+++ src/Grid/Service/ClassDefinitionService.php
@@ -43,7 +43,8 @@ final readonly class ClassDefinitionService implements ClassDefinitionServiceInt
     {
         $classDefinition = $this->getClassDefinition($classId);
 
-        /** @var User $user
+        /** 
+         *  @var User $user
          *  Because Core needs a User
          * */
         $filteredDefinitions = $this->dataObjectServiceResolver->getCustomLayoutDefinitionForGridColumnConfig(
