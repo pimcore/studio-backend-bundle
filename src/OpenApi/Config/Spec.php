07311f5d5a269e1c2294039a7b675158e5d8c58f
Christian Fasching <christian.fasching@pimcore.com>
2025-04-30 06:51:30 +0200

    License Switch to POCL

--- src/OpenApi/Config/Spec.php
+++ src/OpenApi/Config/Spec.php
@@ -2,16 +2,13 @@
 declare(strict_types=1);
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace Pimcore\Bundle\StudioBackendBundle\OpenApi\Config;
@@ -35,8 +32,8 @@ use OpenApi\Attributes\OpenApi;
 )]
 
 #[License(
-    name: 'GNU General Public License version 3 & Pimcore Commercial License',
-    url: 'https://www.pimcore.org/license'
+    name: 'Pimcore Open Core License (POCL)',
+    url: 'https://www.pimcore.com/license'
 )]
 
 final class Spec
