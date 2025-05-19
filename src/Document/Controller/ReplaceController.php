67b80c7202631553dc592d1a269e8031b7fff83d
Lukas Mzigot <30526586+lukmzig@users.noreply.github.com>
2025-05-19 07:49:57 +0200

    [Documents] Replace content (#1082)
    * add document endpoint to replace contents
    
    * Apply php-cs-fixer changes

---
+++ src/Document/Controller/ReplaceController.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Document\Controller;
+
+use OpenApi\Attributes\Post;
+use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
+use Pimcore\Bundle\StudioBackendBundle\Document\Service\ReplaceServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ElementSavingFailedException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ForbiddenException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidElementTypeException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UserNotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Parameter\Path\IdParameter;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\DefaultResponses;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\SuccessResponse;
+use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\ElementTypes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\UserPermissions;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @internal
+ */
+final class ReplaceController extends AbstractApiController
+{
+    private const string ROUTE = '/documents/{sourceId}/replace/{targetId}';
+
+    public function __construct(
+        private readonly ReplaceServiceInterface $replaceService,
+        SerializerInterface $serializer,
+    ) {
+        parent::__construct($serializer);
+    }
+
+    /**
+     * @throws ElementSavingFailedException
+     * @throws ForbiddenException
+     * @throws InvalidElementTypeException
+     * @throws UserNotFoundException
+     * @throws NotFoundException
+     */
+    #[Route(self::ROUTE, name: 'pimcore_studio_api_documents_replace', methods: ['POST'])]
+    #[IsGranted(UserPermissions::DOCUMENTS->value)]
+    #[Post(
+        path: self::PREFIX . self::ROUTE,
+        operationId: 'document_replace_content',
+        description: 'document_replace_content_description',
+        summary: 'document_replace_content_summary',
+        tags: [Tags::Documents->value]
+    )]
+    #[SuccessResponse(description: 'document_replace_content_success_response')]
+    #[IdParameter(type: ElementTypes::TYPE_DOCUMENT, name: 'sourceId')]
+    #[IdParameter(type: ElementTypes::TYPE_DOCUMENT, name: 'targetId')]
+    #[DefaultResponses([
+        HttpResponseCodes::FORBIDDEN,
+        HttpResponseCodes::INTERNAL_SERVER_ERROR,
+        HttpResponseCodes::UNAUTHORIZED,
+        HttpResponseCodes::NOT_FOUND,
+    ])]
+    public function replaceContent(int $sourceId, int $targetId): Response
+    {
+        $this->replaceService->replaceContents($sourceId, $targetId);
+
+        return new Response();
+    }
+}
---
+++ src/Document/Service/ReplaceServiceInterface.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Document\Service;
+
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ElementSavingFailedException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ForbiddenException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidElementTypeException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UserNotFoundException;
+
+interface ReplaceServiceInterface
+{
+    /**
+     * @throws ForbiddenException
+     * @throws ElementSavingFailedException
+     * @throws InvalidElementTypeException
+     * @throws UserNotFoundException
+     * @throws NotFoundException
+     */
+    public function replaceContents(int $sourceId, int $targetId): void;
+}
