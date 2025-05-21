0526e2b0f94756d7155dcb41460d69c4e75ed4f8
Lukas Mzigot <30526586+lukmzig@users.noreply.github.com>
2025-05-21 15:02:09 +0200

    [Documents][Tree] Convert document to different document ype (#1092)
    * convert document type
    
    * Apply php-cs-fixer changes

--- src/Document/Service/ReplaceService.php
+++ src/Document/Service/ReplaceService.php
@@ -14,11 +14,15 @@ declare(strict_types=1);
 namespace Pimcore\Bundle\StudioBackendBundle\Document\Service;
 
 use Exception;
+use Pimcore\Bundle\StaticResolverBundle\Lib\Cache\RuntimeCacheResolverInterface;
+use Pimcore\Bundle\StudioBackendBundle\Document\Util\Trait\DocumentClassTrait;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ElementSavingFailedException;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ForbiddenException;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\InvalidElementTypeException;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
 use Pimcore\Bundle\StudioBackendBundle\Security\Service\SecurityServiceInterface;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\Document\DocumentTypes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\Document\NavigationProperties;
 use Pimcore\Bundle\StudioBackendBundle\Util\Constant\ElementPermissions;
 use Pimcore\Bundle\StudioBackendBundle\Util\Constant\ElementTypes;
 use Pimcore\Model\Document;
@@ -26,17 +30,25 @@ use Pimcore\Model\Document\Folder;
 use Pimcore\Model\Document\PageSnippet;
 use Pimcore\Model\Document\Service as DocumentService;
 use Pimcore\Model\UserInterface;
+use Pimcore\Resolver\ResolverInterface;
+use function in_array;
 
 /**
  * @internal
  */
 final class ReplaceService implements ReplaceServiceInterface
 {
+    use DocumentClassTrait;
+
+    private const array RESTRICTED_PARAMS = ['children', 'siblings', 'scheduledTasks', 'controller', 'template'];
+
     private DocumentService $coreDocumentService;
 
     public function __construct(
         private readonly DocumentServiceInterface $documentService,
+        private readonly ResolverInterface $classResolver,
         private readonly SecurityServiceInterface $securityService,
+        private readonly RuntimeCacheResolverInterface $cacheResolver,
     ) {
         $this->coreDocumentService = new DocumentService();
     }
@@ -59,7 +71,33 @@ final class ReplaceService implements ReplaceServiceInterface
 
             $this->coreDocumentService->copyContents($target, $source);
         } catch (Exception $e) {
-            throw new ElementSavingFailedException($targetId, $e->getMessage());
+            throw new ElementSavingFailedException($targetId, $e->getMessage(), $e);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertType(int $id, string $type): void
+    {
+        $user = $this->securityService->getCurrentUser();
+        $document = $this->getValidDocument($user, $id, true);
+        $newClass = $this->getClassByType($type, $this->classResolver);
+
+        try {
+            /** @var Document $newDocument */
+            $newDocument = new $newClass();
+
+            // overwrite internal store to avoid the "duplicate full path" error
+            $this->cacheResolver->set('document_' . $document->getId(), $newDocument);
+
+            $this->setNewTypeProperties($newDocument, $document);
+            $this->removeNavigationProperties($newDocument, $type);
+
+            $newDocument->setType($type);
+            $newDocument->save();
+        } catch (Exception $e) {
+            throw new ElementSavingFailedException($id, $e->getMessage(), $e);
         }
     }
 
@@ -68,13 +106,33 @@ final class ReplaceService implements ReplaceServiceInterface
      * @throws InvalidElementTypeException
      * @throws NotFoundException
      */
-    private function getValidDocument(UserInterface $user, int $id): Document
+    private function getValidDocument(UserInterface $user, int $id, bool $allowFolder = false): Document
     {
         $element = $this->documentService->getDocumentElement($user, $id);
-        if ($element instanceof Folder) {
+        if (!$allowFolder && $element instanceof Folder) {
             throw new InvalidElementTypeException(ElementTypes::TYPE_FOLDER);
         }
 
         return $element;
+    }
+
+    private function setNewTypeProperties(Document $newDocument, Document $document): void
+    {
+        $props = $document->getObjectVars();
+        foreach ($props as $key => $value) {
+            if (in_array($key, self::RESTRICTED_PARAMS, true)) {
+                continue;
+            }
+            $newDocument->setValue($key, $value);
+        }
+    }
+
+    private function removeNavigationProperties(Document $newDocument, string $type): void
+    {
+        if ($type === DocumentTypes::HARDLINK->value || $type === ElementTypes::TYPE_FOLDER) {
+            foreach (NavigationProperties::values() as $property) {
+                $newDocument->removeProperty('navigation_' . $property);
+            }
+        }
     }
 }
