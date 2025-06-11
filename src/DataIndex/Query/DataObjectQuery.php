9d4813cae582bdfd42dbdbd0d6361520e317ee2d
Martin Eiber <martin.eiber@pimcore.com>
2025-06-11 14:05:32 +0200

    [Task] Add Boolean filter for Data Objects (#1139)
    * bool Filter
    
    * Add Bool Filter
    
    * Add Docs
    
    * Apply php-cs-fixer changes
    
    * Fix stan.
    
    * use ^2.x-dev again.
    
    * Apply php-cs-fixer changes
    
    ---------
    
    Co-authored-by: martineiber <11687066+martineiber@users.noreply.github.com>

--- src/DataIndex/Query/DataObjectQuery.php
+++ src/DataIndex/Query/DataObjectQuery.php
@@ -17,6 +17,7 @@ use Carbon\Carbon;
 use Exception;
 use Pimcore\Bundle\GenericDataIndexBundle\Enum\Search\SortDirection;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\DataObject\DataObjectSearch;
+use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\BooleanFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\ExcludeFoldersFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\IdFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\IdsFilter;
@@ -220,6 +221,13 @@ final class DataObjectQuery implements DataObjectQueryInterface
             $roundToDay,
             $enablePqlFieldNameResolution
         ));
+
+        return $this;
+    }
+
+    public function booleanFilter(string $fieldName, bool $value): self
+    {
+        $this->search->addModifier(new BooleanFilter($fieldName, $value));
 
         return $this;
     }
