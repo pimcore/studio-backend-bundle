50906c553769b1e0cb6ea630a56a6f5fc2364fae
lukmzig <lukas.mzigot@pimcore.com>
2025-06-16 10:58:10 +0200

    fix sonar

--- src/Migrations/Version20250616120000.php
+++ src/Migrations/Version20250616120000.php
@@ -42,10 +42,10 @@ final class Version20250616120000 extends AbstractMigration
 
         $this->addSql('
             INSERT INTO ' . JobRunHidden::TABLE_NAME . ' (jobRunId)
-            SELECT jr.id 
+            SELECT jr.id
             FROM generic_execution_engine_job_run jr
             LEFT JOIN ' . JobRunHidden::TABLE_NAME . " jrh ON jr.id = jrh.jobRunId
-            WHERE jrh.jobRunId IS NULL 
+            WHERE jrh.jobRunId IS NULL
             AND jr.executionContext IN ('studio_stop_on_error', 'studio_continue_on_error')
         ");
     }
