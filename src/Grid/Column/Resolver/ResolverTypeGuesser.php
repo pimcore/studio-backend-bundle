d1998e8e1c8e34a4aa5e352e92c608a3237b7045
martineiber <11687066+martineiber@users.noreply.github.com>
2025-08-25 06:51:54 +0000

    Apply php-cs-fixer changes

--- src/Grid/Column/Resolver/ResolverTypeGuesser.php
+++ src/Grid/Column/Resolver/ResolverTypeGuesser.php
@@ -1,53 +1,60 @@
-<?php 
+<?php
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
 
 namespace Pimcore\Bundle\StudioBackendBundle\Grid\Column\Resolver;
 
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Grid\Schema\ColumnConfiguration;
 use Pimcore\Bundle\StudioBackendBundle\Grid\Service\ColumnConfigurationServiceInterface;
 use Pimcore\Bundle\StudioBackendBundle\Security\Service\SecurityServiceInterface;
-use Pimcore\Bundle\StudioBackendBundle\Grid\Schema\ColumnConfiguration;
 
 class ResolverTypeGuesser implements ResolverTypeGuesserInterface
 {
-
     /**
      * Summary of columnConfigurationCache
+     *
      * @var array<string, ColumnConfiguration[]>
      */
     private array $columnConfigurationCache = [];
 
     /**
      * Summary of columnConfigurationCache
+     *
      * @var string[]
      */
-
     private array $typeChache = [];
-    
+
     public function __construct(
         private readonly ColumnConfigurationServiceInterface $columnConfigurationService,
         private readonly SecurityServiceInterface $securityService,
-    ){}
-
+    ) {
+    }
 
     public function guessType(string $key, string $classId): string
     {
 
         $columnConfigurations = $this->getCollumnConfigurations($classId);
 
-
         return $this->findType($key, $classId, $columnConfigurations);
     }
-
 
     /**
      * @return ColumnConfiguration[]
      */
-    private function getCollumnConfigurations (string $classId): array
+    private function getCollumnConfigurations(string $classId): array
     {
         if (isset($this->columnConfigurationCache[$classId])) {
             return $this->columnConfigurationCache[$classId];
         }
-
 
         $colConfiguration = $this->columnConfigurationService->getAvailableDataObjectColumnConfiguration(
             $classId,
@@ -71,7 +78,6 @@ class ResolverTypeGuesser implements ResolverTypeGuesserInterface
             return $this->typeChache[$chacheName];
         }
 
-
         foreach ($columnConfigurations as $columnConfiguration) {
             if ($columnConfiguration->getKey() === $key) {
                 $this->typeChache[$chacheName] = $columnConfiguration->getType();
@@ -82,5 +88,4 @@ class ResolverTypeGuesser implements ResolverTypeGuesserInterface
 
         throw new NotFoundException('key', $key, 'Column Key');
     }
-
 }
--- src/Grid/Column/Resolver/ResolverTypeGuesserInterface.php
+++ src/Grid/Column/Resolver/ResolverTypeGuesserInterface.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
 namespace Pimcore\Bundle\StudioBackendBundle\Grid\Column\Resolver;
 
 /**
