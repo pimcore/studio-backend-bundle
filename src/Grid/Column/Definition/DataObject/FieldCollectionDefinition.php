c7a6cc58727ba14efc8ef83860007e08c12451d6
Martin Eiber <martin.eiber@pimcore.com>
2025-09-08 15:13:03 +0200

    [Grid] Add field collection definition (#1339)
    * Add Field Collection Definition
    
    * Remove dd

---
+++ src/Grid/Column/Definition/DataObject/FieldCollectionDefinition.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Grid\Column\Definition\DataObject;
+
+/**
+ * @internal
+ */
+final readonly class FieldCollectionDefinition extends AbstractDefinition
+{
+    public function getType(): string
+    {
+        return 'data-object.fieldcollections';
+    }
+
+    public function getFrontendType(): string
+    {
+        return 'fieldcollections';
+    }
+
+    public function isSortable(): bool
+    {
+        return false;
+    }
+
+    public function isFilterable(): bool
+    {
+        return false;
+    }
+}
