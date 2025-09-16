aa6aba6dd0e14e3882d9e56b7b4e52ef31a38c35
Christian F. <christian.feldkirchner@pimcore.com>
2025-09-16 15:51:10 +0200

    Added missing import

--- src/Perspective/Schema/WidgetConfig.php
+++ src/Perspective/Schema/WidgetConfig.php
@@ -16,6 +16,7 @@ namespace Pimcore\Bundle\StudioBackendBundle\Perspective\Schema;
 use OpenApi\Attributes\Property;
 use OpenApi\Attributes\Schema;
 use Pimcore\Bundle\StudioBackendBundle\Response\ElementIcon;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\ElementTypes;
 use Pimcore\Bundle\StudioBackendBundle\Util\Schema\AdditionalAttributesInterface;
 use Pimcore\Bundle\StudioBackendBundle\Util\Trait\AdditionalAttributesTrait;
 
