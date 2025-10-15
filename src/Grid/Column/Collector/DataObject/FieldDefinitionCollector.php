8aee5a5f75d896bc3128c08017b0dc051191e0d0
Lukas Mzigot <30526586+lukmzig@users.noreply.github.com>
2025-10-15 13:52:50 +0200

    Update src/Grid/Column/Collector/DataObject/FieldDefinitionCollector.php
    Co-authored-by: Martin Eiber <martin.eiber@pimcore.com>

--- src/Grid/Column/Collector/DataObject/FieldDefinitionCollector.php
+++ src/Grid/Column/Collector/DataObject/FieldDefinitionCollector.php
@@ -112,7 +112,7 @@ final class FieldDefinitionCollector implements
                 continue;
             }
 
-            // Use ObjectbricksCollector
+            // When definition is instance of ObjectBrick there is a ObjectbricksCollector. We also skip all field types in skipFieldTypes
             if (!$definition instanceof Data || $definition instanceof Objectbricks ||
                 in_array($definition->getFieldType(), $this->skipFieldTypes, true)
             ) {
