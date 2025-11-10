ca8f4114ba8ce41145584c7b77d6d3060c7ea3e1
stunnerparas <prsdahal@gmail.com>
2025-11-10 12:17:41 +0000

    Progressing GDPR

---
+++ src/Gdpr/MappedParameter/GdprStructuredSearchRequest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ * @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Gdpr\MappedParameter;
+
+use Pimcore\Bundle\StudioBackendBundle\Gdpr\Attribute\Request\SearchTerms;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Valid;
+
+/**
+ * @internal
+ */
+final readonly class GdprStructuredSearchRequest
+{
+    /**
+     * @param string[] $providers
+     */
+    public function __construct(
+        #[NotBlank]
+        #[All(new Type('string'))]
+        public array $providers,
+
+        #[Valid]
+        public ?SearchTerms $searchTerms = null
+    ) {
+    }
+}
