9aa9666eba42a2cb88a03649a5a464bd8522dff5
stunnerparas <prsdahal@gmail.com>
2025-11-18 10:14:59 +0000

    type fix docs

--- src/Gdpr/Schema/GdprSearchResult.php
+++ src/Gdpr/Schema/GdprSearchResult.php
@@ -16,6 +16,7 @@ namespace Pimcore\Bundle\StudioBackendBundle\Gdpr\Schema;
 use OpenApi\Attributes\Items;
 use OpenApi\Attributes\Property;
 use OpenApi\Attributes\Schema;
+use Pimcore\Bundle\StudioBackendBundle\Gdpr\Schema\GdprDataRow;
 
 /**
  * @internal
