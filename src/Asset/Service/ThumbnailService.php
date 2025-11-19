0fb2b2150878ce6c88503b4698a050ba60769fb5
Marco <89011527+mcop1@users.noreply.github.com>
2025-11-19 15:02:52 +0100

    [Assets] Fix custom thumbnails for SVG (#1526)
    * Added fix format configuration for source and print thumbnail formats
    
    * Use enums instead of literals
    
    * Adapted code to work with and without config parameters

--- src/Asset/Service/ThumbnailService.php
+++ src/Asset/Service/ThumbnailService.php
@@ -82,15 +82,16 @@ final readonly class ThumbnailService implements ThumbnailServiceInterface
             }
         }
 
-        if ($parameter === null) {
-            return $image->getThumbnail($thumbnailConfig);
+        $format = $thumbnailConfig->getFormat();
+        if ($parameter !== null) {
+            $thumbnailConfig = $this->setThumbnailConfigCropParameter($parameter, $thumbnailConfig);
+            $format = $parameter->getMimeType() ?: $format;
         }
 
         $thumbnailConfig = $this->setThumbnailConfigFormatParameter(
-            $parameter->getMimeType() ?: $thumbnailConfig->getFormat(),
+            $format,
             $thumbnailConfig
         );
-        $thumbnailConfig = $this->setThumbnailConfigCropParameter($parameter, $thumbnailConfig);
 
         return $image->getThumbnail($thumbnailConfig);
     }
@@ -391,6 +392,7 @@ final readonly class ThumbnailService implements ThumbnailServiceInterface
             return $thumbnailConfig;
         }
 
+        $mimeType = strtolower($mimeType);
         if ($mimeType === MimeTypes::SOURCE->value || $mimeType === MimeTypes::PRINT->value) {
             $mimeType = MimeTypes::PNG->value;
         }
