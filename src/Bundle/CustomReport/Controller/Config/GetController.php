2e49ca7400a4e62ab6c9324d64e6dbbf5feb962f
Marco Perberschlager <marco.perberschlager@pimcore.com>
2025-11-21 12:30:56 +0100

    Added voter for voting on having one permission in a set of permissions

--- src/Bundle/CustomReport/Controller/Config/GetController.php
+++ src/Bundle/CustomReport/Controller/Config/GetController.php
@@ -25,8 +25,9 @@ use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\DefaultResponses;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\SuccessResponse;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Config\Tags;
+use Pimcore\Bundle\StudioBackendBundle\Security\PermissionsToCheck;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\CustomReportPermissions;
 use Pimcore\Bundle\StudioBackendBundle\Util\Constant\HttpResponseCodes;
-use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -51,11 +52,6 @@ final class GetController extends AbstractApiController
      * @throws Exception
      */
     #[Route(self::ROUTE, name: 'pimcore_studio_api_custom_reports_report', methods: ['GET'])]
-    #[IsGranted(
-        new Expression(
-            'is_granted("reports") or is_granted("reports_permissions")'
-        )
-    )]
     #[Get(
         path: self::PREFIX . self::ROUTE,
         operationId: 'custom_reports_report',
@@ -76,6 +72,14 @@ final class GetController extends AbstractApiController
     ])]
     public function getByName(string $name): JsonResponse
     {
+        $this->denyAccessUnlessGranted(
+            'HasOneOf',
+            new PermissionsToCheck([
+                CustomReportPermissions::REPORTS_CONFIG->value,
+                CustomReportPermissions::REPORTS->value,
+            ])
+        );
+
         return $this->jsonResponse(
             $this->customReportService->getCustomReportDetails($name)
         );
---
+++ src/Security/PermissionsToCheck.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\StudioBackendBundle\Security;
+
+use InvalidArgumentException;
+
+final readonly class PermissionsToCheck
+{
+    public function __construct(
+        private array $permissionsToCheck
+    ) {
+        if(empty($this->permissionsToCheck)) {
+            throw new InvalidArgumentException('Permissions to check must not be empty');
+        }
+    }
+
+    public function getPermissionsToCheck(): array
+    {
+        return $this->permissionsToCheck;
+    }
+}
