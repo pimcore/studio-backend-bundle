dd914174eccc8f5544889f082e406a6971bf6e27
stunnerparas <prsdahal@gmail.com>
2025-11-27 10:29:33 +0000

    ci fix

--- src/Gdpr/Provider/Legacy/ObjectExporter.php
+++ src/Gdpr/Provider/Legacy/ObjectExporter.php
@@ -21,9 +21,8 @@ use Pimcore\Normalizer\NormalizerInterface;
 
 /**
  * Copied from old admin-ui-classic-bundle
- * https://github.com/pimcore/admin-ui-classic-bundle/blob/9258d42920dbb475badc1adea59a7552ab089ac4/src/GDPR/DataProvider/Exporter.php#L32
- * Use with caution, this is a copy from the admin-ui-classic-bundle
- *
+ * https://github.com/pimcore/admin-ui-classic-bundle/blob/9258d42920dbb475badc1adea59a7552ab089ac4/src/GDPR/
+ * DataProvider/Exporter.php#L32
  * @internal
  */
 final readonly class ObjectExporter implements ObjectExporterInterface
