ef73aaee12518877375cfa6e4f4f68f431a876e9
stunnerparas <prsdahal@gmail.com>
2025-11-27 13:16:32 +0000

    ci issue fix

--- src/Gdpr/Attribute/Request/SearchTerms.php
+++ src/Gdpr/Attribute/Request/SearchTerms.php
@@ -29,22 +29,44 @@ use Symfony\Component\Validator\Constraints\Type;
 final readonly class SearchTerms
 {
     public function __construct(
-        #[Property(description: 'The ID to search for.', type: 'int', nullable: true, example: 3)]
+        #[Property(
+            description: 'The ID to search for.',
+            type: 'int',
+            nullable: true,
+            example: 3
+        )]
         #[Type('int')]
         private ?int $id = null,
 
-        #[Property(description: 'The first name to search for. ', type: 'string', nullable: true, example: 'John')]
+        #[Property(
+            description: 'The first name to search for.',
+            type: 'string',
+            nullable: true,
+            example: 'John'
+        )]
         #[Type('string')]
         private ?string $firstname = null,
 
-        #[Property(description: 'The last name to search for.', type: 'string', nullable: true, example: 'Doe')]
+        #[Property(
+            description: 'The last name to search for.',
+            type: 'string',
+            nullable: true,
+            example: 'Doe'
+        )]
         #[Type('string')]
         private ?string $lastname = null,
 
-        #[Property(description: 'The email address to search for.', type: 'string', nullable: true, example: 'john.doe@example.com')]
+        #[Property(
+            description: 'The email address to search for.',
+            type: 'string',
+            nullable: true,
+            example: 'john.doe@example.com'
+        )]
         #[Type('string')]
         private ?string $email = null,
-    ) {
+    )
+    
+    {
         if ($this->id === null &&
             $this->firstname === null &&
             $this->lastname === null &&
