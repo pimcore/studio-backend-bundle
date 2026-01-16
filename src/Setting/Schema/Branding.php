5a2ca9e2db7292143352a95ee5e289549902d151
martineiber <11687066+martineiber@users.noreply.github.com>
2026-01-16 07:10:53 +0000

    Apply php-cs-fixer changes

--- src/Setting/Schema/Branding.php
+++ src/Setting/Schema/Branding.php
@@ -25,7 +25,7 @@ use OpenApi\Attributes\Schema;
         'colorAdminInterface',
         'colorAdminInterfaceBackground',
         'loginScreenCustomImage',
-        'loginScreenCustomBackgroundImage'
+        'loginScreenCustomBackgroundImage',
     ],
     type: 'object'
 )]
