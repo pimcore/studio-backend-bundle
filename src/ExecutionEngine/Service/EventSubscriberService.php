2c7b190520ada71a15659ddb819a81ba7977836b
Martin Eiber <martin.eiber@pimcore.com>
2026-02-09 14:33:32 +0100

    [Execution Engine] Do not send Notification (#1646)
    * Remove sendNotification
    
    * Remove sendNotification
    
    * Apply php-cs-fixer changes
    
    ---------
    
    Co-authored-by: martineiber <11687066+martineiber@users.noreply.github.com>

--- src/ExecutionEngine/Service/EventSubscriberService.php
+++ src/ExecutionEngine/Service/EventSubscriberService.php
@@ -23,8 +23,6 @@ use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UserNotFoundException;
 use Pimcore\Bundle\StudioBackendBundle\Mercure\Schema\ExecutionEngine\Finished;
 use Pimcore\Bundle\StudioBackendBundle\Mercure\Service\PublishServiceInterface;
 use Pimcore\Bundle\StudioBackendBundle\Mercure\Service\UserTopicServiceInterface;
-use Pimcore\Bundle\StudioBackendBundle\Notification\Schema\SendNotificationParameters;
-use Pimcore\Bundle\StudioBackendBundle\Notification\Service\SendNotificationServiceInterface;
 
 /**
  * @internal
@@ -34,7 +32,6 @@ final readonly class EventSubscriberService implements EventSubscriberServiceInt
     public function __construct(
         private JobRunErrorLogRepositoryInterface $jobRunErrorLogRepository,
         private PublishServiceInterface $publishService,
-        private SendNotificationServiceInterface $sendNotificationService,
         private UserTopicServiceInterface $userTopicService,
     ) {
 
@@ -55,18 +52,6 @@ final readonly class EventSubscriberService implements EventSubscriberServiceInt
             $event->getJobName(),
             $event->getJobRunOwnerId(),
             $event->getNewState()
-        );
-
-        $payload = $this->publishService->getJsonData($finished);
-        $parameters = new SendNotificationParameters(
-            $event->getJobRunOwnerId(),
-            $topic,
-            $topic
-        );
-        $this->sendNotificationService->sendNotification(
-            $parameters,
-            null,
-            $payload
         );
         $this->publishService->publish(
             $topic,
