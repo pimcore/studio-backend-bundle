af132e4c8218ca88134603158ce3a3d2784c8bf6
lukmzig <lukas.mzigot@pimcore.com>
2026-04-03 07:58:39 +0200

    fix: PHP Stan

--- src/User/Controller/UploadUserImageController.php
+++ src/User/Controller/UploadUserImageController.php
@@ -16,10 +16,9 @@ namespace Pimcore\Bundle\StudioBackendBundle\User\Controller;
 use OpenApi\Attributes\Post;
 use OpenApi\Attributes\Property;
 use Pimcore\Bundle\StudioBackendBundle\Controller\AbstractApiController;
-use Pimcore\Bundle\StudioBackendBundle\Exception\Api\DatabaseException;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ForbiddenException;
 use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
-use Pimcore\Bundle\StudioBackendBundle\Exception\Api\ParseException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\UserNotFoundException;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Parameter\Path\IdParameter;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Request\MultipartFormDataRequestBody;
 use Pimcore\Bundle\StudioBackendBundle\OpenApi\Attribute\Response\DefaultResponses;
@@ -49,7 +48,7 @@ final class UploadUserImageController extends AbstractApiController
     }
 
     /**
-     * @throws NotFoundException|DatabaseException|ForbiddenException|ParseException
+     * @throws ForbiddenException|NotFoundException|UserNotFoundException
      */
     #[Route('/user/upload-image/{id}', name: 'pimcore_studio_api_user_upload_image', methods: ['POST'])]
     #[Post(
@@ -74,6 +73,7 @@ final class UploadUserImageController extends AbstractApiController
     #[DefaultResponses([
         HttpResponseCodes::NOT_FOUND,
         HttpResponseCodes::FORBIDDEN,
+        HttpResponseCodes::UNAUTHORIZED,
     ])]
     public function uploadUserImage(
         int $id,
