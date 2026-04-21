7e821f41d633d66702d18b25005da2dd65cd2905
Martin Eiber <martin.eiber@pimcore.com>
2026-04-21 10:22:22 +0200

    [DataObjects][Calculated Values] getCalculatedValueForEditMode not considered (#1764)
    * Calculated value
    
    * Calculated value Classification and ObjectBrick
    
    * update requirement to static resolver
    
    * Change composer static resolver
    
    * Add Doc for DetailDataInterface

--- src/DataObject/Data/Adapter/CalculatedValueAdapter.php
+++ src/DataObject/Data/Adapter/CalculatedValueAdapter.php
@@ -13,11 +13,16 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StudioBackendBundle\DataObject\Data\Adapter;
 
+use Pimcore\Bundle\StudioBackendBundle\DataObject\Data\DetailDataInterface;
 use Pimcore\Bundle\StudioBackendBundle\DataObject\Data\Model\FieldContextData;
 use Pimcore\Bundle\StudioBackendBundle\DataObject\Data\SetterDataInterface;
+use Pimcore\Bundle\StaticResolverBundle\Models\DataObject\DataObjectServiceResolverInterface;
 use Pimcore\Bundle\StudioBackendBundle\DataObject\Service\DataAdapterLoaderInterface;
 use Pimcore\Model\DataObject\ClassDefinition\Data;
+use Pimcore\Model\DataObject\ClassDefinition\Data\CalculatedValue as CalculatedValueDefinition;
 use Pimcore\Model\DataObject\Concrete;
+use Pimcore\Model\DataObject\Data\CalculatedValue;
+use Pimcore\Model\DataObject\Objectbrick\Data\AbstractData;
 use Pimcore\Model\UserInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
@@ -25,8 +30,21 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
  * @internal
  */
 #[AutoconfigureTag(DataAdapterLoaderInterface::ADAPTER_TAG)]
-final readonly class CalculatedValueAdapter implements SetterDataInterface
+final readonly class CalculatedValueAdapter implements SetterDataInterface, DetailDataInterface
 {
+    private const string OWNER_TYPE_OBJECT = 'object';
+
+    private const string OWNER_TYPE_LOCALIZED_FIELD = 'localizedfield';
+
+    private const string OWNER_TYPE_OBJECT_BRICK = 'objectbrick';
+
+    private const string LOCALIZED_FIELDS_NAME = 'localizedfields';
+
+    public function __construct(
+        private DataObjectServiceResolverInterface $dataObjectServiceResolver,
+    ) {
+    }
+
     public function getDataForSetter(
         Concrete $element,
         Data $fieldDefinition,
@@ -37,5 +55,73 @@ final readonly class CalculatedValueAdapter implements SetterDataInterface
         bool $isPatch = false
     ): null {
         return null;
+    }
+
+    public function getDetailData(
+        Concrete $object,
+        mixed $value,
+        Data $fieldDefinition,
+        ?FieldContextData $contextData = null,
+    ): ?string {
+        if (!$fieldDefinition instanceof CalculatedValueDefinition) {
+            return null;
+        }
+
+        $calculatedValue = new CalculatedValue($fieldDefinition->getName());
+        $this->applyContextualData($calculatedValue, $fieldDefinition, $contextData);
+
+        return $this->dataObjectServiceResolver->getCalculatedFieldValueForEditMode(
+            $object,
+            [],
+            $calculatedValue,
+        );
+    }
+
+    private function applyContextualData(
+        CalculatedValue $calculatedValue,
+        CalculatedValueDefinition $fieldDefinition,
+        ?FieldContextData $contextData,
+    ): void {
+        $contextObject = $contextData?->getContextObject();
+
+        if ($contextObject instanceof AbstractData) {
+            $calculatedValue->setContextualData(
+                self::OWNER_TYPE_OBJECT_BRICK,
+                $contextObject->getFieldname(),
+                $contextObject->getType(),
+                $fieldDefinition->getName(),
+                null,
+                null,
+                $fieldDefinition,
+            );
+
+            return;
+        }
+
+        $language = $contextData?->getLanguage();
+
+        if ($language !== null) {
+            $calculatedValue->setContextualData(
+                self::OWNER_TYPE_LOCALIZED_FIELD,
+                self::LOCALIZED_FIELDS_NAME,
+                null,
+                $language,
+                null,
+                null,
+                $fieldDefinition,
+            );
+
+            return;
+        }
+
+        $calculatedValue->setContextualData(
+            self::OWNER_TYPE_OBJECT,
+            $fieldDefinition->getName(),
+            null,
+            null,
+            null,
+            null,
+            $fieldDefinition,
+        );
     }
 }
