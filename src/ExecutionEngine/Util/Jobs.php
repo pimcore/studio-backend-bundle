cd107b1dff6d9b20d5ea2426db2931fd7f206c9e
markus-moser <markus.moser@pimcore.com>
2026-05-05 12:47:47 +0000

    [Feature]: Add distinct job names for folder patch and folder export operations
    - Split COLLECT_EXPORT_FOLDER_ELEMENTS into COLLECT_CSV_FOLDER_EXPORT_ELEMENTS and COLLECT_XLSX_FOLDER_EXPORT_ELEMENTS so CSV and XLSX folder exports get individual job names matching their create-job counterparts
    - Add PATCH_FOLDER_ELEMENTS so folder batch-edit runs can be distinguished from single-element patch runs
    - Update PatchSubscriber to publish the Mercure finished event for both PATCH_ELEMENTS and PATCH_FOLDER_ELEMENTS
    - Update FolderCollectionSubscriber and ExportService to use the new format-specific job names
    - Add migration Version20260505124233 to hide any existing job runs of the new job types

--- src/ExecutionEngine/Util/Jobs.php
+++ src/ExecutionEngine/Util/Jobs.php
@@ -21,9 +21,11 @@ enum Jobs: string
     case UPLOAD_ASSETS = 'studio_ee_job_upload_assets';
     case ZIP_FILE_UPLOAD = 'studio_ee_job_upload_zip_file';
     case CREATE_CSV = 'studio_ee_job_create_csv';
-    case COLLECT_EXPORT_FOLDER_ELEMENTS = 'studio_ee_job_collect_folder_export_elements';
+    case COLLECT_CSV_FOLDER_EXPORT_ELEMENTS = 'studio_ee_job_collect_csv_folder_export_elements';
+    case COLLECT_XLSX_FOLDER_EXPORT_ELEMENTS = 'studio_ee_job_collect_xlsx_folder_export_elements';
     case CREATE_XLSX = 'studio_ee_job_create_xlsx';
     case PATCH_ELEMENTS = 'studio_ee_job_patch_elements';
+    case PATCH_FOLDER_ELEMENTS = 'studio_ee_job_patch_folder_elements';
     case CLONE_DATA_OBJECTS = 'studio_ee_job_clone_data_objects';
     case REWRITE_REFERENCES = 'studio_ee_job_rewrite_element_references';
     case DELETE_DATA_OBJECTS = 'studio_ee_job_delete_data_objects';
