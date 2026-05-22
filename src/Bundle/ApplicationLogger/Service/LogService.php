842bdb144a87b742fca2451bf4913be6a944a60f
lukmzig <lukas.mzigot@pimcore.com>
2026-05-22 11:25:13 +0200

    add missing endpoint to stream log file content

--- src/Bundle/ApplicationLogger/Service/LogService.php
+++ src/Bundle/ApplicationLogger/Service/LogService.php
@@ -13,14 +13,23 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StudioBackendBundle\Bundle\ApplicationLogger\Service;
 
+use League\Flysystem\FilesystemException;
 use Pimcore\Bundle\ApplicationLoggerBundle\Enum\LogLevel;
+use Pimcore\Bundle\StaticResolverBundle\Models\Tool\StorageResolverInterface;
 use Pimcore\Bundle\StudioBackendBundle\Bundle\ApplicationLogger\Event\PreResponse\LogEntryEvent;
 use Pimcore\Bundle\StudioBackendBundle\Bundle\ApplicationLogger\Hydrator\LogHydratorInterface;
 use Pimcore\Bundle\StudioBackendBundle\Bundle\ApplicationLogger\Repository\LogRepositoryInterface;
 use Pimcore\Bundle\StudioBackendBundle\Bundle\ApplicationLogger\Schema\LogEntry;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\NotFoundException;
+use Pimcore\Bundle\StudioBackendBundle\Exception\Api\StreamResourceNotFoundException;
 use Pimcore\Bundle\StudioBackendBundle\MappedParameter\CollectionFilterParameter;
 use Pimcore\Bundle\StudioBackendBundle\Response\Collection;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\Asset\MimeTypes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\HttpResponseCodes;
+use Pimcore\Bundle\StudioBackendBundle\Util\Constant\HttpResponseHeaders;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use function sprintf;
 
 /**
  * @internal
@@ -31,6 +40,7 @@ final readonly class LogService implements LogServiceInterface
         private EventDispatcherInterface $eventDispatcher,
         private LogHydratorInterface $hydrator,
         private LogRepositoryInterface $logRepository,
+        private StorageResolverInterface $storageResolver,
     ) {
     }
 
@@ -63,6 +73,31 @@ final readonly class LogService implements LogServiceInterface
     public function listPriorities(): array
     {
         return array_column(LogLevel::cases(), 'value');
+    }
+
+    public function streamFileObject(string $filePath): StreamedResponse
+    {
+        $storage = $this->storageResolver->get('application_log');
+
+        try {
+            if (!$storage->fileExists($filePath)) {
+                throw new NotFoundException('File object', $filePath, 'filePath');
+            }
+
+            $stream = $storage->readStream($filePath);
+        } catch (FilesystemException $e) {
+            throw new StreamResourceNotFoundException(
+                sprintf('Could not read file object at path "%s": %s', $filePath, $e->getMessage())
+            );
+        }
+
+        return new StreamedResponse(
+            static function () use ($stream) {
+                echo stream_get_contents($stream);
+            },
+            HttpResponseCodes::SUCCESS->value,
+            [HttpResponseHeaders::HEADER_CONTENT_TYPE->value => MimeTypes::PLAIN_TEXT->value]
+        );
     }
 
     private function getHydratedLogs(array $log): LogEntry
