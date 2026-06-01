67145bd5ad8ae5b4e09d7def7037eea4819ad6a2
lukmzig <lukas.mzigot@pimcore.com>
2026-06-01 16:53:17 +0200

    fix classname column too short

--- src/Installer.php
+++ src/Installer.php
@@ -184,7 +184,7 @@ final class Installer extends SettingsStoreAwareInstaller
 
         $table->addColumn('classId', 'string', [
             'notnull' => false,
-            'length' => 10,
+            'length' => 50,
         ]);
 
         $table->addForeignKeyConstraint(
@@ -269,7 +269,7 @@ final class Installer extends SettingsStoreAwareInstaller
             'unsigned' => true,
         ]);
 
-        $table->addColumn('classId', 'string', ['notnull' => false, 'length' => 10]);
+        $table->addColumn('classId', 'string', ['notnull' => false, 'length' => 50]);
 
         $table->addColumn(
             'owner',
